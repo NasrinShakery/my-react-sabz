6785bb6ec68d9e26cf429e9f3e6097e7ee759410
NasrinShakery <nasrin.shakery8@gmail.com>
2023-10-24 23:23:34 +0330

    Done

--- src/components/BookList/AddForm.js
+++ src/components/BookList/AddForm.js
@@ -1,57 +1,126 @@
-import React, { Component } from 'react'
-import Book from './Book'
+import React, { Component } from "react";
+import Book from "./Book";
 
 export default class AddForm extends Component {
+   constructor() {
+      super();
 
-    constructor() {
-        super()
+      this.state = {
+         books: [],
 
-        this.state = {
-            books: [],
+         title: "",
+         author: "",
+         year: "",
+      };
+   }
 
-            title: '',
-            author: '',
-            year: '',
+   titleChangeHandler(e) {
+        this.setState({
+            title: e.target.value
+        })
+   }
+   authorChangeHandler(e) {
+        this.setState({
+            author: e.target.value
+        })
+    }
+   
+   yearChangeHandler(e) {
+        this.setState({
+            year: e.target.value
+        })
+   }
+
+   submitHandler (e) {
+        e.preventDefault();
+        let {title, author, year} = this.state;
+        
+        if(title && author && year){
+
+            let newBook = {
+                id : this.state.books.length +1,
+                title,
+                author,
+                year
+            }
+
+            this.setState({
+                books :[...this.state.books, newBook]
+            })
+
+            this.setState({
+                title : "",
+                author : "",
+                year : "",
+            })
         }
+   }
 
-    }
+   render() {
+      return (
+         <>
+            <form id="book-form" autoComplete="off" onSubmit={(e)=> this.submitHandler(e)}>
+               <div className="form-group">
+                  <label htmlFor="title">Title</label>
+                  <input
+                     type="text"
+                     id="title"
+                     className="form-control"
+                     onChange={(e) => {
+                        this.titleChangeHandler(e);
+                     }}
+                     value={this.state.title}
+                  />
+               </div>
 
+               <div className="form-group">
+                  <label htmlFor="author">Author</label>
+                  <input
+                     type="text"
+                     id="author"
+                     className="form-control"
+                     onChange={(e) => {
+                        this.authorChangeHandler(e);
+                     }}
+                     value={this.state.author}
+                  />
+               </div>
 
-    render() {
-        return (
-            <>
-                <form id="book-form" autocomplete="off">
-                    <div className="form-group">
-                        <label for="title">Title</label>
-                        <input type="text" id="title" className="form-control" />
-                    </div>
-
-                    <div className="form-group">
-                        <label for="author">Author</label>
-                        <input type="text" id="author" className="form-control" />
-                    </div>
-
-                    <div className="form-group">
-                        <label for="year">Year</label>
-                        <input type="text" id="year" className="form-control" />
-                    </div>
-                    <input type="submit" value="Add Book" className="btn btn-warning btn-block add-btn" />
-                </form>
-                <table class="table table-striped mt-5 text-center">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Year</th>
-                        </tr>
-                    </thead>
-                    <tbody id="book-list">
-                        <Book />
-                    </tbody>
-                </table>
-
-
-            </>
-        )
-    }
+               <div className="form-group">
+                  <label htmlFor="year">Year</label>
+                  <input
+                     type="text"
+                     id="year"
+                     className="form-control"
+                     onChange={(e) => {
+                        this.yearChangeHandler(e);
+                     }}
+                     value={this.state.year}
+                  />
+               </div>
+               <input
+                  type="submit"
+                  value="Add Book"
+                  className="btn btn-warning btn-block add-btn"
+               />
+            </form>
+            <table className="table table-striped mt-5 text-center">
+               <thead>
+                  <tr>
+                     <th>Title</th>
+                     <th>Author</th>
+                     <th>Year</th>
+                  </tr>
+               </thead>
+               <tbody id="book-list">
+                {
+                    this.state.books.map((book) =>(
+                        <Book {...book} key={book.id}/>
+                    ))
+                }
+               </tbody>
+            </table>
+         </>
+      );
+   }
 }
