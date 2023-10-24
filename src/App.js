91c812b93f580aac57ff8c932defe99e49d4b6ae
NasrinShakery <nasrin.shakery8@gmail.com>
2023-10-24 19:39:59 +0330

    Add BookList project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React, { Component } from 'react'
+import Header  from './components/Header/Header'
+import AddForm from './components/BookList/AddForm'
 
-function App() {
-  return (
-    <>
-      App
-    </>
-  );
+export default class App extends Component {
+
+    render() {
+        return (
+            <div className='container mt-4'>
+                <Header></Header>
+                <AddForm></AddForm>
+            </div>
+        )
+    }
 }
-
-export default App;
