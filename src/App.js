f571d3687d9189303c634afba797189ff993bb63
NasrinShakery <nasrin.shakery8@gmail.com>
2023-11-12 23:39:35 +0330

    fetch (get) Users Data from firebase DB

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Login from "./components/Login"
+// import Login from "./components/Login" 
 import Login1 from "./components/Login1"
+import Users from './components/Users';
 
 function App() {
   return (
     <>
       {/* <Login/>   WRONG!! */} 
-      <Login1/>
+      {/* <Login1/> */}
+
+      <Users/>
+
     </>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { object } from "prop-types";
+import React, { useState, useEffect } from "react";
+import Table from 'react-bootstrap/Table';
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+   const [users, setUsers] = useState([]);
 
-export default Users
+   useEffect(() => {
+      fetch("https://project11-60413-default-rtdb.firebaseio.com/users.json")
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(Object.entries(data));
+            setUsers(Object.entries(data));
+         });
+   }, []);
+   return (
+      <Table striped bordered hover>
+         <thead>
+            <tr>
+               <th>#</th>
+               <th>First Name</th>
+               <th>Last Name</th>
+               <th>Username</th>
+            </tr>
+         </thead>
+         <tbody>
+          {users.map((user) =>(
+            <tr>
+               <td>#</td>
+               <td>{user[1].fname}</td>
+               <td>{user[1].lname}</td>
+               <td>{user[1].email}</td>
+            </tr>
+          ))}
+            
+         </tbody>
+      </Table>
+   );
+};
+
+export default Users;
