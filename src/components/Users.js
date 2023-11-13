73c358504e360143e761dcfb31869d76c22bcc66
NasrinShakery <nasrin.shakery8@gmail.com>
2023-11-13 10:59:21 +0330

    a note in spp for VPN

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,10 @@
 import { object } from "prop-types";
 import React, { useState, useEffect } from "react";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 
 const Users = () => {
    const [users, setUsers] = useState([]);
+   const [err, setErr] = useState("");
 
    useEffect(() => {
       fetch("https://project11-60413-default-rtdb.firebaseio.com/users.json")
@@ -11,30 +12,44 @@ const Users = () => {
          .then((data) => {
             console.log(Object.entries(data));
             setUsers(Object.entries(data));
+         })
+         .catch((error) => {
+            setErr(error);
          });
    }, []);
+
    return (
-      <Table striped bordered hover>
-         <thead>
-            <tr>
-               <th>#</th>
-               <th>First Name</th>
-               <th>Last Name</th>
-               <th>Username</th>
-            </tr>
-         </thead>
-         <tbody>
-          {users.map((user) =>(
-            <tr>
-               <td>#</td>
-               <td>{user[1].fname}</td>
-               <td>{user[1].lname}</td>
-               <td>{user[1].email}</td>
-            </tr>
-          ))}
-            
-         </tbody>
-      </Table>
+      <>
+         {err ? (
+            <>
+               <h1 style={{ color: "red" }}> Turn On Your * VPN * !!</h1>
+               <p>{err}</p>
+            </>
+         ) : (
+            <>
+               <Table striped bordered hover>
+                  <thead>
+                     <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {users.map((user) => (
+                        <tr>
+                           <td>#</td>
+                           <td>{user[1].fname}</td>
+                           <td>{user[1].lname}</td>
+                           <td>{user[1].email}</td>
+                        </tr>
+                     ))}
+                  </tbody>
+               </Table>
+            </>
+         )}
+      </>
    );
 };
 
